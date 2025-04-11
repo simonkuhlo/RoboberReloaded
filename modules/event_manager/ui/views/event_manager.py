c39728457fc355d9b1c9abeb7c3fd8ca29633ca1
Gander Simon <siga241@gmail.com>
2025-04-11 09:53:16 +0200

    Make Edit button persistent

--- modules/event_manager/ui/views/event_manager.py
+++ modules/event_manager/ui/views/event_manager.py
@@ -4,6 +4,6 @@ from ..items import edit_start
 
 class View(discord.ui.View):
     def __init__(self):
-        super().__init__(timeout=600)
+        super().__init__(timeout=None)
         # addd buttons
         self.add_item(edit_start.Button())
